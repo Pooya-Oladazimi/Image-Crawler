f21d108128fed40da6a7c6eace4344543feaa27c
Pooya <pooya.oladazimi@gmail.com>
2020-07-16 13:04:58 +0200

    added exception handling in run.py for empty inputs, updated gitignore

--- run.py
+++ run.py
@@ -1,8 +1,14 @@
 import socket, ssl, sys
 from imageCrawler import Crawler
 
+if len(sys.argv) == 1:
+    print("You need to specify a web page url as the target!")
+    sys.exit()
 target = sys.argv[1]
-limit = sys.argv[2]
+limit = 20
+if len(sys.argv) == 3:
+    limit = sys.argv[2]
+
 crawl = Crawler(socket, str(target), ssl, int(limit))
 crawl.main()
 
